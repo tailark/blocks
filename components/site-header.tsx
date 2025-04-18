5a3374a338c50704159bc08d0818cb32afed13d5
Meschac Irung <meschacirung@gmail.com>
2025-04-18 18:07:22 +0200

    fix :Invalid DOM property . Did you mean ?

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -70,7 +70,7 @@ export const SiteHeader = () => {
                                 viewBox="0 0 14 14"
                                 className="size-4">
                                 <g fill="none">
-                                    <g clip-path="url(#primeTwitter0)">
+                                    <g clipPath="url(#primeTwitter0)">
                                         <path
                                             fill="currentColor"
                                             d="M11.025.656h2.147L8.482 6.03L14 13.344H9.68L6.294 8.909l-3.87 4.435H.275l5.016-5.75L0 .657h4.43L7.486 4.71zm-.755 11.4h1.19L3.78 1.877H2.504z"
