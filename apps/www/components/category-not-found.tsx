11df7a04c65e5996b5744e1831ed2d528fa07248
Meschac Irung <meschacirung@gmail.com>
2025-05-06 11:48:55 +0200

    Enhance category not found UI with improved border beams and layout adjustments

--- apps/www/components/category-not-found.tsx
+++ apps/www/components/category-not-found.tsx
@@ -13,8 +13,11 @@ export default function CategoryNotFound({ categoryName, kitName }: { categoryNa
 
     return (
         <section className="bg-muted mb-6 flex min-h-[calc(100dvh-6rem)] flex-col items-center justify-center gap-4 border-b py-16 text-center md:mb-12 dark:bg-transparent">
-            <div className="flex gap-4 [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
-                <div className="rounded-(--radius) bg-background size-32 -translate-x-1/3 translate-y-1/2 border shadow-md">
+            <div
+                aria-hidden
+                className="-mt-16 flex gap-4 [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]">
+                <div className="rounded-(--radius) size-32 -translate-x-1/3 translate-y-1/2">
+                    <div className="rounded-(--radius) bg-background absolute inset-0 border shadow-md"></div>
                     <BorderBeam
                         size={100}
                         duration={12}
@@ -22,11 +25,12 @@ export default function CategoryNotFound({ categoryName, kitName }: { categoryNa
                         className="from-transparent via-indigo-400 to-sky-300"
                     />
                 </div>
-                <div className="rounded-(--radius) bg-background relative size-32 overflow-hidden border shadow-md">
+                <div className="rounded-(--radius) relative size-32 overflow-hidden">
+                    <div className="rounded-(--radius) bg-background absolute inset-0 border shadow-md"></div>
                     <BorderBeam
                         size={200}
                         duration={12}
-                        className="via-foreground/50 from-transparent to-transparent"
+                        className="from-transparent via-lime-400 to-transparent"
                     />
                 </div>
             </div>
