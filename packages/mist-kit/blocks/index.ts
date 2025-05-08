3767c337c9166b23a985d143893dd7ef20f6b3b9
Meschac Irung <meschacirung@gmail.com>
2025-05-08 16:06:34 +0200

    feat: add features block with task management UI and reusable card component

--- packages/mist-kit/blocks/index.ts
+++ packages/mist-kit/blocks/index.ts
@@ -3,3 +3,6 @@ export { default as HeroSectionOne } from './hero-section/one'
 export { default as HeroSectionTwo } from './hero-section/two'
 export { default as HeroSectionThree } from './hero-section/three'
 export { default as HeroSectionFour } from './hero-section/four'
+
+// Features Blocks
+export { default as FeaturesOne } from './features/one'
