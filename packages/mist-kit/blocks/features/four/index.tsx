6db6ed4eafcea492d47f4d25ab3b9fdcd52af4a2
Meschac Irung <meschacirung@gmail.com>
2025-05-12 08:19:00 +0200

    refactor: migrate core package to vitejs

--- packages/mist-kit/blocks/features/four/index.tsx
+++ packages/mist-kit/blocks/features/four/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { cn } from '@tailark/core/lib/utils'
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
+import { ToggleGroup, ToggleGroupItem } from '@tailark/core/ui/toggle-group'
 import { Bold, Calendar1, Ellipsis, Italic, Strikethrough, Underline } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
