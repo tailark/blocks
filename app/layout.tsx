22ce37f19f897149030cad25ec3c38b6bd3eacb3
Meschac Irung <meschacirung@gmail.com>
2026-06-15 10:25:45 +0800

    refactor: enhance layout structure with theme provider and font integration
    Updated the RootLayout component to include a ThemeProvider for theming support and integrated Google Fonts. Adjusted HTML structure for hydration handling and applied utility classes for  styling.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
-import type { Metadata } from "next"
-import "./globals.css"
+import type { Metadata } from 'next'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
+import { Geist_Mono, Geist } from 'next/font/google'
+
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
+
+const fontMono = Geist_Mono({
+    subsets: ['latin'],
+    variable: '--font-mono',
+})
 
 export const metadata: Metadata = {
-    title: "Tailark Registry",
-    description: "Open source shadcn/ui registry for Tailark blocks and components.",
+    title: 'Tailark Registry',
+    description: 'Open source shadcn/ui registry for Tailark blocks and components.',
 }
 
 export default function RootLayout({
@@ -12,8 +22,13 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body>{children}</body>
+        <html
+            lang="en"
+            suppressHydrationWarning
+            className={cn('antialiased', fontMono.variable, 'font-sans', geist.variable)}>
+            <body>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
         </html>
     )
 }
