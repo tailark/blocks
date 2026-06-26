3d74f8c442716871deb3c5c89f00349fc38b5f45
Meschac Irung <meschacirung@gmail.com>
2026-06-26 11:32:13 +0800

    refactor: update call-to-action components for improved layout and content
    Enhanced the call-to-action components by updating the layout for better responsiveness and visual appeal. Removed the obsolete 'three' variant and refined the content in the 'one' and 'two' variants, including updated headings and button links for a more engaging user experience.

--- registry/bases/radix/dusk/blocks/call-to-action/two.tsx
+++ registry/bases/radix/dusk/blocks/call-to-action/two.tsx
@@ -3,30 +3,16 @@ import Link from 'next/link'
 
 export default function CallToAction() {
     return (
-        <section className="py-16">
-            <div className="mx-auto max-w-5xl rounded-3xl border px-6 py-12 md:py-20 lg:py-32">
-                <div className="text-center">
-                    <h2 className="text-balance text-4xl font-semibold lg:text-5xl">Start Building</h2>
-                    <p className="mt-4">Libero sapiente aliquam quibusdam aspernatur.</p>
+        <section className="py-16 md:py-32">
+            <div className="mx-auto max-w-7xl px-6">
+                <div className="flex items-center justify-center gap-6 max-lg:flex-col max-lg:text-center lg:items-end lg:justify-between">
+                    <h2 className="max-w-4xl text-balance text-5xl font-semibold tracking-tight xl:text-6xl">Start building from here</h2>
 
-                    <div className="mt-12 flex flex-wrap justify-center gap-4">
-                        <Button
-                            asChild
-                            size="lg">
-                            <Link href="/">
-                                <span>Get Started</span>
-                            </Link>
-                        </Button>
-
-                        <Button
-                            asChild
-                            size="lg"
-                            variant="outline">
-                            <Link href="/">
-                                <span>Book Demo</span>
-                            </Link>
-                        </Button>
-                    </div>
+                    <Button
+                        asChild
+                        size="lg">
+                        <Link href="#">Get Started</Link>
+                    </Button>
                 </div>
             </div>
         </section>
